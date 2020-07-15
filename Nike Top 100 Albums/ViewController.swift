41afb54aa6e308dc270b66f314c2f5ea748001c5
Taylor Paul <tpaul@cbtnuggets.com>
2020-07-15 16:27:26 -0700

    Initial Commit

---
+++ Nike Top 100 Albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nike Top 100 Albums
+//
+//  Created by Taylor Paul on 7/15/20.
+//  Copyright © 2020 Taylor Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
